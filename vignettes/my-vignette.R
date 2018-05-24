25530ff564c284144bc6af72c370d61d653f23aa
Gidon Cohen <gidon.cohen@durham.ac.uk>
2018-05-24 23:30:43 +0100

    Restore the vignettes

---
+++ vignettes/my-vignette.R
@@ -0,0 +1,34 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----message=FALSE, warning=FALSE----------------------------------------
+library(durhamevp)
+
+## also using tidyverse functions
+library(tidyverse)
+
+## ----eval=FALSE----------------------------------------------------------
+#  con <- evdb_connect()
+
+## ------------------------------------------------------------------------
+con <- evdb_connect(password_method="config")
+
+
+## ------------------------------------------------------------------------
+assign_initalsets_to_users(con, 3)
+
+## ------------------------------------------------------------------------
+assign_initalsets_to_users(con, c(3, 4, 5, 6))
+
+## ------------------------------------------------------------------------
+get_allocation(con, user_id = 1)%>%
+  dplyr::select(user_id, document_id, allocation_type, allocation_date) 
+
+
+## ------------------------------------------------------------------------
+get_allocation(con, user_id = 1, allocation_type="ideal") %>%
+  dplyr::select(user_id, document_id, allocation_type, allocation_date) 
+
