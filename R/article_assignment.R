5bb6866d05585e2b3b5b0370d72f364a4724e41c
Gidon Cohen <gidon.cohen@durham.ac.uk>
2018-06-14 11:11:02 +0100

    Implementation of automatic (hidden) management of database connections in function (through manage_dbcons() function.

--- R/article_assignment.R
+++ R/article_assignment.R
@@ -1,16 +1,18 @@
 
-assign_article_to_user <- function (con, document_id, user_id, allocation_type, allocated_by, allocation_date=as.character(Sys.Date()), status="NEW", coding_complete=0){
+assign_article_to_user <- function (document_id, user_id, allocation_type, allocated_by, allocation_date=as.character(Sys.Date()), status="NEW", coding_complete=0, con="make", password_method="keyring"){
   #' Assign article to a user.
   #'
   #' \code{assign_article_to_user} assigns a specific article or set of articles to a specific user or set of users. Other document assignment functions are convenience wrappers around this funciton with arguments set appropriately.
-  #' @param con The database connection to the election violence database.
   #' @param document_id Id of the document to be assigned or a vector of document ids.
   #' @param user_id Id of the user the document is to be assigned to or a vector of user ids.
   #' @param allocation_type Type of allocation (training, testing, coding, checking, ideal).
   #' @param allocation_date Date allocation made (usually today).
   #' @param status Status of document coding (generally 'NEW' for newly assigned documents).
+  #' @param con The database connection to the election violence database (this argument is ignored in current implementations with database connections handled silently within the functions).
+  #' @param password_method (this argument is ignored in current implementations with database connections handled silently within the functions).
   #' @export
 
+  con <- manage_dbcons()
 
   allocated_at <- as.character(Sys.time())
   for (this_user_id in user_id){
@@ -35,7 +37,7 @@ assign_article_to_user <- function (con, document_id, user_id, allocation_type, 
   return(NULL)
 }
 
-assign_set <- function(con, user_id, set, allocation_type, allocated_by = "assign_set", status="NEW"){
+assign_set <- function(user_id, set, allocation_type, allocated_by = "assign_set", status="NEW"){
   #' Assigns set of articles to user
   #'
   #' \code{assign_set} assigns a whole set of articles to a user (articles which are already allocated to a user are not allocated again).
@@ -43,13 +45,15 @@ assign_set <- function(con, user_id, set, allocation_type, allocated_by = "assig
   #' @describeIn assign_article_to_user Assign set of articles to user
 
   # restrict assignment of set to items not already allocated to the user
-  items_needed <- user_allocated_fromset(con, user_id, set=set, not_allocated = TRUE)
+
+  con <- manage_dbcons()
+
+  items_needed <- user_allocated_fromset(user_id, set=set, not_allocated = TRUE)
 
 
 
   for (document_id in items_needed){
-    assign_article_to_user(con,
-                           document_id=document_id,
+    assign_article_to_user(document_id=document_id,
                            user_id=user_id,
                            allocation_date=as.character(Sys.Date()),
                            allocation_type=allocation_type,
@@ -63,7 +67,7 @@ assign_set <- function(con, user_id, set, allocation_type, allocated_by = "assig
   message(completion_string)
 }
 
-assign_testset_to_user<-function(con, user_id, allocation_type="testing", allocated_by="assign_testset_to_user") {
+assign_testset_to_user<-function(user_id, allocation_type="testing", allocated_by="assign_testset_to_user") {
   #' Assign test set
   #'
   #' \code{assign_testset_to_user} assigns the test set of articles to a user specified by the user id. Articles which are already allocated to a user are not allocated again.
@@ -71,11 +75,11 @@ assign_testset_to_user<-function(con, user_id, allocation_type="testing", alloca
   #' @describeIn assign_article_to_user Assigns testset to user or users
 
   testset=durhamevp::define_testset()
-  assign_set(con=con, user_id=user_id, set=testset, allocation_type=allocation_type, allocated_by=allocated_by)
+  assign_set(user_id=user_id, set=testset, allocation_type=allocation_type, allocated_by=allocated_by)
 }
 
 
-assign_trainingset_to_user <- function(con, user_id, allocation_type="training", allocated_by="assign_trainingset_to_user"){
+assign_trainingset_to_user <- function(user_id, allocation_type="training", allocated_by="assign_trainingset_to_user"){
   #' Assign training set to user
   #'
   #' \code{assign_trainingset_to_user} assigns the training set to user (articles which are already allocated to a user are not allocated again).
@@ -83,10 +87,10 @@ assign_trainingset_to_user <- function(con, user_id, allocation_type="training",
   #' @describeIn assign_article_to_user Assigns training set to user
 
   trainingset=define_trainingset()
-  assign_set(con=con, user_id=user_id, set=trainingset, allocation_type=allocation_type, allocated_by=allocated_by)
+  assign_set(user_id=user_id, set=trainingset, allocation_type=allocation_type, allocated_by=allocated_by)
 }
 
-assign_initalsets_to_users <- function(con, user_ids){
+assign_initalsets_to_users <- function(user_ids){
   #' Assigns initial article sets to user
   #'
   #' \code{assign_initalsets_to_users} assigns the training set and test sets to a user or set of users (articles which are already allocated to a user are not allocated again).
@@ -97,18 +101,17 @@ assign_initalsets_to_users <- function(con, user_ids){
 
 
   for (user_id in user_ids){
-    assign_testset_to_user(con, user_id)
-    assign_trainingset_to_user(con, user_id)
+    assign_testset_to_user(user_id=user_id)
+    assign_trainingset_to_user(user_id=user_id)
   }
 }
 
 
-allocate_randomly <- function (con, user_ids, set, coder_rate=1.1, allocation_type="coding", allocated_by="regular_random_assignment", restrict_to_actual=TRUE, make_assignments=TRUE){
+allocate_randomly <- function (user_ids, set, coder_rate=1.1, allocation_type="coding", allocated_by="regular_random_assignment", restrict_to_actual=TRUE, make_assignments=TRUE){
   #' Randomly assign a set of articles amongst a set of users for coding
   #'
   #' \code{allocate_randomly} assigns a set of articles amongst a set of users for them to code in the election violence database.
   #' By default the articles and user ids are checked against the database and codes which do not correspond to existing documents and/or users are ignored.
-  #' @param con The database connection to the election violence database.
   #' @param user_ids The users to allocate the articles amongst (vector with single or multiple user_ids).
   #' @param set The set of documents which are to be allocated (vector with single or multiple of document_ids).
   #' @param coder_rate The average number of coders to be allocated to a document.
@@ -116,13 +119,14 @@ allocate_randomly <- function (con, user_ids, set, coder_rate=1.1, allocation_ty
   #' @param allocated_by The value to write to the allocated_by field in the database document_allocations table.
   #' @param restrict_to_actual Should the restriction to actual users and documents be enforced. Should only be set to FALSE for debugging purposes.
   #' @param make_assignments Actually make changes to the database (TRUE) or only create a proposed set of changes as a dataframe (FALSE)
-  #'
   #' @export
+
+  con <- manage_dbcons()
 
   # restrict to actual articles and actual users (both must already be in the database)
   if(restrict_to_actual){
-    set<-documents_to_actual(con, set)
-    user_ids<-users_to_actual(con, user_ids)
+    set<-documents_to_actual(set, con=con, password_method=password_method)
+    user_ids<-users_to_actual(user_ids, con=con, password_method=password_method)
   }
   min_coders <- floor(coder_rate)
   additional_coder_rate <- coder_rate - min_coders
@@ -135,7 +139,7 @@ allocate_randomly <- function (con, user_ids, set, coder_rate=1.1, allocation_ty
 
   for (this_article in set){
     n_assignments = min_coders + stats::rpois(1, additional_coder_rate)
-    users_already_coding <- get_allocation(con, document_id=this_article)$user_id
+    users_already_coding <- get_allocation(document_id=this_article, con=con, password_method=password_method)$user_id
     available_users <- user_ids [!user_ids %in% users_already_coding]
     if (length(available_users)< n_assignments) {
       warning(paste0("insufficient available users: for document ", this_article, " aim to code document ", n_assignments, " times with only ", length(available_users), " coder(s) available. No assignments made for this document."))
@@ -153,25 +157,27 @@ allocate_randomly <- function (con, user_ids, set, coder_rate=1.1, allocation_ty
     }
   }
   if(make_assignments){
-    allocate_from_df(con, assign_dat, allocation_type, allocated_by)
+    allocate_from_df(assign_dat, allocation_type, allocated_by)
   }
   assign_dat
 }
 
-allocate_from_df<-function(con, assign_dat, allocation_type, allocated_by){
+allocate_from_df<-function(assign_dat, allocation_type, allocated_by){
   #' Assign users to documents from a dataframe
   #'
   #' Assign users to documents from a dataframe where dataframe has a user_id column and and a document_id column
-  #' @param con The connection to the election violence database
+
   #' @param assign_dat A dataframe (or tibble) with a user_id column and a document_id column
   #' @param allocation_type The allocation type to write to the database
   #' @param allocated_by The allocated by to write to the database
   #' @export
 
+
   apply(assign_dat, 1, function (x)
-    assign_article_to_user(con,
+    assign_article_to_user(
                            x[["document_id"]],
                            x[["user_id"]],
                            allocation_type = allocation_type,
-                           allocated_by = allocated_by))
+                           allocated_by = allocated_by
+                           ))
 }
--- R/database_interactivity.R
+++ R/database_interactivity.R
@@ -1,4 +1,48 @@
+keyring_evp_available<-function(username="not implemented"){
 
+  #' Checks if an evp password is available.
+  #'
+  #' Checks if an evp password is available on this computer.
+  #' @export
+  these_keys<-keyring::key_list()
+  needed_key<- dplyr::filter(these_keys, service=="evp", username=="data_writer")
+  if(nrow(needed_key)==0){
+    available <- FALSE
+  } else if(nrow(needed_key)>0){
+    available <- TRUE
+  } else {
+    warning(paste0("There are an unexpected number of entries in the keyring: ", nrow(needed_key)))
+    available <- FALSE
+  }
+
+  return(available)
+}
+
+manage_dbcons <- function(){
+  #' Returns a connection to the election violence database
+  #'
+  #' This function is primarily for use within other functions.
+  #' Returns an active connection to the the election violence database, choosing the first open valid connection to the database. If there is no open valid connection to the database then a connection will be made. Running the function will also close all invalid (e.g. expired) database connections.
+  #' Database password is obtained from the keyring if there is a password for the database in the keyring. Otherwise the user is prompted for the database password.
+  #' @export
+  if(keyring_evp_available()){
+    password_method <- "keyring"
+  } else {
+    password_method <- "ask"
+  }
+  all_cons <- DBI::dbListConnections(RMySQL::MySQL())
+  valid_idx <- sapply(all_cons, DBI::dbIsValid)
+
+  for (invalid_con in all_cons[!valid_idx]){
+    DBI::dbDisconnect(invalid_con)
+  }
+  if(length(valid_idx)>0){
+    con <- all_cons[valid_idx][[1]]
+  } else {
+    con <- evdb_connect(password_method = password_method)
+  }
+  con
+}
 evdb_connect <- function(host="coders.victorianelectionviolence.uk", user="data_writer", dbname="evp", port=3306, password_method="ask"){
   #' Set connection to election violence database
   #'
@@ -13,7 +57,7 @@ evdb_connect <- function(host="coders.victorianelectionviolence.uk", user="data_
   #' # for default connection to the database
   #' con <- evdb_connect()
   #' # using alternative method of entering password information
-  #' con <- evdb_connect(password_method="config")
+  #' con <- evdb_connect(password_method="keyring")
 
   if (password_method=="keyring"){
     password <- keyring::key_get(dbname, user)
@@ -66,14 +110,14 @@ expand_out_ors <- function(col_name, vals){
   res
 }
 
-get_allocation <- function(con, user_id = "all", allocation_type="all", document_id="all"){
+get_allocation <- function(user_id = "all", allocation_type="all", document_id="all"){
   #' Get the articles currently allocated to the user.
-  #' @param con The database connection to .
   #' @param user_id The user_id to check in the database.
   #' @param document_id The set of document_ids to include in the allocation
   #' @return dataframe of the document allocations to the user.
   #' @export
 
+  con <- manage_dbcons()
   this_sql <-"SELECT * FROM portal_userdocumentallocation" # base query
 
   res<-build_where_condition("user_id", user_id, this_sql, NULL)
@@ -91,24 +135,26 @@ get_allocation <- function(con, user_id = "all", allocation_type="all", document
   allocation
 }
 
-user_allocated_fromset <- function(con, user_id, set, not_allocated=FALSE){
+user_allocated_fromset <- function(user_id, set, not_allocated=FALSE){
   #' Which items allocated (or not allocated) to user
   #'
   #' Returns the elements of a set of documents which are allocated (or not allocated) to a user.
-  #' @param con The database connection to the election violence database.
   #' @param user_id The user id to check in the database.
   #' @param set The set of documents to check.
   #' @param not_allocated If FALSE (default) returns the items allocated to the user, if TRUE returns items NOT allocated to the user.
   #' @return A vector numbers representing the document ids from the set which are allocated to the user (or which are not allocated to the user if not_allocated=TRUE).
   #' @examples
   #' # returns the trainingset documents allocated to user 1
-  #' user_allocated_fromset(con, 1, set=define_trainingset())
+  #' user_allocated_fromset(1, set=define_trainingset())
   #'
   #' # returns the trainingset documents not allocated to user 1
-  #' user_allocated_fromset(con, 1, set=define_trainingset(), not_allocated=TRUE)
+  #' user_allocated_fromset(1, set=define_trainingset(), not_allocated=TRUE)
   #'
   #' @export
-  user_allocation <- get_allocation(con, user_id)
+
+  con <- manage_dbcons()
+
+  user_allocation <- get_allocation(user_id)
   training_allocation <- dplyr::filter(user_allocation, allocation_type=="training")
 
   doc_ids <- set[set %in% user_allocation$document_id] # document ids which are in training set but not allocated to the user
@@ -123,11 +169,13 @@ user_allocated_fromset <- function(con, user_id, set, not_allocated=FALSE){
 }
 
 
-get_user <- function(con, user_id){
+get_user <- function(user_id){
   #' Gets user details.
-  #' @param con The connection to the election violence database.
+
   #' @param user_id The user id.
   #' @export
+
+  con <- manage_dbcons()
 
   this_sql<-"SELECT * FROM auth_user"
 
@@ -143,7 +191,9 @@ get_user <- function(con, user_id){
   users
 }
 
-get_status<-function(con, user_id){
+get_status<-function(user_id){
+  con <- manage_dbcons()
+
   this_sql<-"SELECT * FROM auth_user WHERE id=?user_id ;"
 
   this_safe_sql<-DBI::sqlInterpolate(DBI::ANSI(), this_sql, user_id=user_id)
@@ -153,12 +203,14 @@ get_status<-function(con, user_id){
 
 }
 
-get_document<-function(con, document_id){
+get_document<-function(document_id){
   #' Returns the document table filtered by document id
   #'
-  #' @param con The connection to the election violence database.
   #' @param document_id Document id or ids to filter by.
   #' @export
+
+  con <- manage_dbcons()
+
   this_sql<-"SELECT * FROM portal_document" # base query
 
   res<-build_where_condition("id", document_id, this_sql, NULL)
@@ -169,15 +221,18 @@ get_document<-function(con, document_id){
                                      .dots = interpolate_list)
   documents<-DBI::dbGetQuery(con, this_safe_sql)
 
+
   documents
 }
 
-get_candidate_documents<-function(con, cand_document_id){
+get_candidate_documents<-function(cand_document_id){
   #' Returns the document table filtered by document id
   #'
   #' @param con The connection to the election violence database.
   #' @param cand_document_id Document id or ids to filter by.
   #' @export
+
+  con <- manage_dbcons()
   this_sql<-"SELECT * FROM portal_candidatedocument" # base query
 
   res<-build_where_condition("id", cand_document_id, this_sql, NULL)
@@ -191,9 +246,10 @@ get_candidate_documents<-function(con, cand_document_id){
   cand_documents
 }
 
-users_to_actual<-function(con, user_id){
+users_to_actual<-function(user_id){
   #' @export
-  users_from_db<-get_user(con, user_id)$id
+  con <- manage_dbcons()
+  users_from_db<-get_user(user_id)$id
   actual_users <- user_id[user_id %in% users_from_db]
   if (length(actual_users)<length(user_id)){
     warning(paste0("Some user ids not found in database and will not be used: ", paste0(user_id[!user_id %in% actual_users], collapse=", ")))
@@ -202,10 +258,11 @@ users_to_actual<-function(con, user_id){
   actual_users
 }
 
-documents_to_actual<-function(con, document_id){
+documents_to_actual<-function(document_id){
   #' @export
 
-  docs_from_db<-get_document(con, document_id)$id
+  con <- manage_dbcons()
+  docs_from_db<-get_document(document_id)$id
   actual_docs <- document_id[document_id %in% docs_from_db]
   if (length(actual_docs)<length(document_id)){
     warning(paste0("Some document ids not found in database and will not be used: ", paste0(document_id[!document_id %in% actual_docs], collapse=", ")))
@@ -214,13 +271,14 @@ documents_to_actual<-function(con, document_id){
   actual_docs
 }
 
-get_user_mode <- function(con, user_id="all"){
+get_user_mode <- function(user_id="all"){
   #' Returns table of current user modes
   #'
   #' Returns the table of current user modes.
-  #' @param con The connection to the election violence database.
   #' @param user_id Id of the user whose mode to check or a vector of user ids. The default ("all") selects all users.
   #' @export
+
+  con <- manage_dbcons()
   this_sql<-"SELECT * FROM portal_userprofile"
 
   res<-build_where_condition("user_id", user_id, this_sql, NULL)
@@ -235,18 +293,21 @@ get_user_mode <- function(con, user_id="all"){
   profiles
 }
 
-set_user_mode <- function (con, user_id, new_mode){
+set_user_mode <- function (user_id, new_mode){
   #' Sets user mode
   #'
   #' Sets the users mode to a new value (training, testing, coding, checking, ideal).
-  #' @param con The connection to the election violence database.
   #' @param user_id Id of the user whose mode to set or a vector of user ids.
   #' @param new_mode The new mode (one of: training, testing, coding, checking, ideal) to set as the users mode(s). For multiple users either a single value should be given to be assigned to all identified users, or a vector of values should be given of the same length as the vector of user_ids.
   #' @export
+
+  con <- manage_dbcons()
+  #print(con)
+
   if(length(new_mode)==1) new_mode <- rep(new_mode, length(user_id))
   if(length(new_mode)!=length(user_id)) warning("length (mode) not equal to length(user_id))")
 
-  current_modes <- get_user_mode(con, user_id) %>%
+  current_modes <- get_user_mode(user_id) %>%
     mutate(new_mode=new_mode)
   for (this_user in user_id){
     this_data <- current_modes %>%
@@ -263,4 +324,16 @@ set_user_mode <- function (con, user_id, new_mode){
 
 
   }
+}
+
+killDbConnections <- function () {
+
+  all_cons <- DBI::dbListConnections(RMySQL::MySQL())
+
+  print(all_cons)
+
+  for(con in all_cons) DBI::dbDisconnect(con)
+
+  print(paste(length(all_cons), " connections killed."))
+
 }
