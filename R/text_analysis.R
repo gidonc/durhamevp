84faf0ca28378c0d90890a00ccc5e346f0887f37
Gidon Cohen <gidon.cohen@durham.ac.uk>
2018-06-21 08:02:38 +0100

    Add missing quanteda namespace to functions.

--- R/text_analysis.R
+++ R/text_analysis.R
@@ -24,10 +24,10 @@ preprocess_corpus<-function(the_corpus, min_termfreq=2, min_docfreq=2,
   #' Preprocess a text corpus and return a document feature matrix (wrapper round quanteda functions).
   #' @param the_corpus The text corpus to split.
   #' @export
-  the_dfm <- dfm(the_corpus, stem=TRUE, remove=stopwords("english"), remove_punct=remove_punct, remove_numbers=remove_numbers, remove_hyphens=remove_hyphens)
-  the_dfm <- dfm_trim(the_dfm, min_termfreq=min_termfreq, min_docfreq = min_docfreq)
+  the_dfm <- quanteda::dfm(the_corpus, stem=TRUE, remove=quanteda::stopwords("english"), remove_punct=remove_punct, remove_numbers=remove_numbers, remove_hyphens=remove_hyphens)
+  the_dfm <- quanteda::dfm_trim(the_dfm, min_termfreq=min_termfreq, min_docfreq = min_docfreq)
   if(dfm_tfidf){
-    the_dfm<-dfm_tfidf(the_dfm)
+    the_dfm<-quanteda::dfm_tfidf(the_dfm)
   }
   the_dfm
 }
@@ -38,7 +38,7 @@ split_dfm <- function(dfm, n_train){
   #' @param n_train The number of documents in the training set.
   #' @export
   training_set <- quanteda::dfm_sample(dfm, n_train)
-  testing_set <- dfm[setdiff(docnames(dfm), docnames(training_set))]
+  testing_set <- dfm[setdiff(quanteda::docnames(dfm), quanteda::docnames(training_set))]
   return(list(training_set=training_set, testing_set=testing_set))
 }
 
@@ -48,18 +48,18 @@ split_corpus<-function(the_corpus, n_train, min_termfreq=2, min_docfreq=2,
   #' Preprocess a text corpus and divide it into training and testing sets based on number of training items.
   #' Note: it is more efficient to preprocess and split separately, especially if running in a loop.
   #' @export
-  the_dfm <- dfm(the_corpus, stem=TRUE, remove=stopwords("english"), remove_punct=remove_punct, remove_numbers=remove_numbers, remove_hyphens=remove_hyphens)
-  the_dfm <- dfm_trim(the_dfm, min_termfreq=min_termfreq, min_docfreq = min_docfreq)
+  the_dfm <- quanteda::dfm(the_corpus, stem=TRUE, remove=quanteda::stopwords("english"), remove_punct=remove_punct, remove_numbers=remove_numbers, remove_hyphens=remove_hyphens)
+  the_dfm <- quanteda::dfm_trim(the_dfm, min_termfreq=min_termfreq, min_docfreq = min_docfreq)
   if(dfm_tfidf){
-    the_dfm<-dfm_tfidf(the_dfm)
+    the_dfm<-quanteda::dfm_tfidf(the_dfm)
   }
-  training_set <- dfm_sample(the_dfm, n_train)
-  testing_set <- the_dfm[setdiff(docnames(the_dfm), docnames(training_set))]
+  training_set <- quanteda::dfm_sample(the_dfm, n_train)
+  testing_set <- the_dfm[setdiff(quanteda::docnames(the_dfm), quanteda::docnames(training_set))]
   return(list(training_set=training_set, testing_set=testing_set))
 }
 my_naivebayes<-function(train, test){
   #' @export
-  nb<-textmodel_nb(train, y=docvars(train, "EV_article"), prior="docfreq")
+  nb<-quanteda::textmodel_nb(train, y=quanteda::docvars(train, "EV_article"), prior="docfreq")
   prob_nb<-predict(nb, newdata = test, type="probability")
   pred_nb<-data.frame(predict(nb, newdata = test, type="class"))
   res<-data.frame(predict_nb=pred_nb, prob_nb)
@@ -69,21 +69,21 @@ my_naivebayes<-function(train, test){
 
 my_affinity<-function(train, test){
   #' @export
-  aff<-textmodel_affinity(train, y=docvars(train, "EV_article"))
+  aff<-quanteda::textmodel_affinity(train, y=quanteda::docvars(train, "EV_article"))
   pred_aff<-predict(aff, newdata = test)
 }
 
 my_wordscores<-function(train, test){
   #' @export
-  this_wordscores<-textmodel_wordscores(train, y=docvars(train, "EV_article"))
-  pred_wordscores<-predict(this_wordscores, newdata = test, force=TRUE)
+  this_wordscores<-quanteda::textmodel_wordscores(train, y=quanteda::docvars(train, "EV_article"))
+  pred_wordscores<-quanteda::predict(this_wordscores, newdata = test, force=TRUE)
   pred_wordscores
 
 }
 organise_results <- function (testset, pred_nb, pred_aff=NULL, pred_wordscores=NULL){
   #' @export
-  this_res<-as.tbl(data.frame(actual=docvars(testset, "EV_article"),
-                              actual_f=factor(docvars(testset, "EV_article")),
+  this_res<-as.tbl(data.frame(actual=quanteda::docvars(testset, "EV_article"),
+                              actual_f=factor(quanteda::docvars(testset, "EV_article")),
                               nb_ev_cat=pred_nb$predict_nb,
                               nb_ev_prob=pred_nb$prob_ev,
                               nb_notev_prob=pred_nb$prob_notev
@@ -115,7 +115,7 @@ assess_classification <- function(res_data){
   if("aff_ev_cat_f" %in% names(res_data)){
     aff_assess<-as.data.frame(caret::confusionMatrix(data=this_res$aff_ev_cat_f, reference=this_res$actual_f, mode="everything", positive="1")$byClass)
     names(aff_assess)<-"value"
-    aff_assess <- rownames_to_column(aff_assess) %>%
+    aff_assess <- tibble::rownames_to_column(aff_assess) %>%
       mutate(model="affinity")
   } else{
     aff_assess<-data.frame(matrix(nrow=0, ncol=3))
