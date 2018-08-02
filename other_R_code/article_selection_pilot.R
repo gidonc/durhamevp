e4cd769bc329ef2fa9a39d0676819da593cf38b9
Gidon Cohen <gidon.cohen@durham.ac.uk>
2018-08-02 10:54:55 +0100

    Further write up of article selection pilot

--- other_R_code/article_selection_pilot.R
+++ other_R_code/article_selection_pilot.R
@@ -18,7 +18,6 @@ classdocs<-classdocs %>%
   dplyr::mutate(unclass=0, classified=1)
 
 ##----initial.1832.results----
-head(classdocs)
 all_searches<-get_archivesearches()
 initial_1832_searches<-all_searches%>%
   dplyr::select(id, search_text, archive_date_start, archive_date_end) %>%
@@ -42,58 +41,92 @@ all_docs<- bind_rows(classdocs,
                      unclass_i_1832)
 all_docs$fakeid<-1:nrow(all_docs)
 
+##----descriptives.1832----
+res_32_des<-res_i_1832 %>%
+  group_by(url) %>%
+  mutate(search_text=sub(" ", "_", search_text)) %>%
+  select(url, search_text) %>%
+  mutate(value=1) %>%
+  spread(search_text, value, fill=0) %>%
+  mutate(tot_art=nrow(.))
+
+knitr::kable(res_32_des %>%
+  group_by(election_riot, election_incident, election_disturbance) %>%
+  summarize(n=length(tot_art), prop=n/tot_art[1])%>%
+    arrange(-prop), digits = 2)
+
+knitr::kable(res_32_des %>%
+               group_by(election_riot) %>%
+               summarize(n=length(tot_art), prop=n/tot_art[1]) %>%
+               arrange(-election_riot),
+             digits = 2)
+
+knitr::kable(res_32_des %>%
+               group_by(election_incident) %>%
+               summarize(n=length(tot_art), prop=n/tot_art[1])%>%
+               arrange(-election_incident),
+             digits = 2)
+
+knitr::kable(res_32_des %>%
+               group_by(election_disturbance) %>%
+               summarize(n=length(tot_art), prop=n/tot_art[1])%>%
+               arrange(-election_disturbance),
+             digits = 2)
+
+
 ##----first.stage.classification----
 all_corpus_d<-corpus(all_docs[c("fakeid", "description", "EV_article", "classified")], text_field="description")
-all_corpus_o<-corpus(all_docs[c("fakeid", "ocr", "EV_article", "classified")], text_field = "ocr")
 
 all_dfm_d <- preprocess_corpus(all_corpus_d, stem=TRUE, min_termfreq=20, min_docfreq = 20)
-all_dfm_o <- preprocess_corpus(all_corpus_o, stem=TRUE, min_termfreq=20, min_docfreq = 20)
 
 class_dfm_d<-quanteda::dfm_subset(all_dfm_d, quanteda::docvars(all_dfm_d, "classified")==1)
-class_dfm_o<-quanteda::dfm_subset(all_dfm_o, quanteda::docvars(all_dfm_d, "classified")==1)
 class_nb <- quanteda::textmodel_nb(class_dfm_d, y=quanteda::docvars(class_dfm_d, "EV_article"), prior="uniform")
-keywords<-nb_keywords(class_dfm_d, "EV_article")
-head(keywords, 20)
-tail(keywords, 20)
+first_stage_keywords<-nb_keywords(class_dfm_d, "EV_article")
 
 ##----second.stage.classification----
 S_dfm <- quanteda::dfm_subset(all_dfm_d, quanteda::docvars(all_dfm_d, "classified")==0)
 quanteda::docvars(S_dfm, "T")<-predict(class_nb, newdata = S_dfm, type="class")
+second_stage_keywords<-nb_keywords(S_dfm, "T")
 
-king_stage2 <- quanteda::textmodel_nb(S_dfm, y=quanteda::docvars(S_dfm, "T"), distribution="Bernoulli")
-king_stage2 <- nb_keywords(S_dfm, "T")
-knitr::kable(head(king_stage2, 30))
 
-names(king_stage2)<-c("rowname", "stage2_0", "stage2_1", "stage2_id")
-names(keywords)<-c("rowname", "stage1_0", "stage1_1", "stage1_id")
-p_change<-left_join(king_stage2, keywords, by="rowname") %>%
-  mutate(logit_stage1 =log(stage1_1/stage1_0), logit_stage2=log(stage2_1/stage2_0)) %>%
-  mutate(abs_logit_change=abs(logit_stage1-logit_stage2)) %>%
-  mutate(logit_change=logit_stage2-logit_stage1) %>%
-  arrange(logit_change)
+##----join.stages----
+names(second_stage_keywords)<-c("keyword", "stg2_0", "stg2_1", "id")
+names(first_stage_keywords)<-c("keyword", "stg1_0", "stg1_1", "id")
+p_change<-left_join(first_stage_keywords, second_stage_keywords, by=c("keyword", "id")) %>%
+  mutate(lgt_stg1 =log(stg1_1/stg1_0), lgt_stg2=log(stg2_1/stg2_0)) %>%
+  mutate(abs_lgt_chng=abs(lgt_stg1-lgt_stg2)) %>%
+  mutate(lgt_chng=lgt_stg2-lgt_stg1) %>%
+  arrange(lgt_chng)
 
 p_change%>%
-  ggplot(aes(logit_stage1, logit_stage2))+
-  geom_point()
+  ggplot(aes(lgt_stg1, lgt_stg2))+
+  geom_point() +
+  ggtitle("Classifier Word-Level Classification Probability (logit transformed) in \n Stage 1 v Stage 2 Classifier")
 
 ##----classification.results----
-head(keywords, 30)
-head(king_stage2, 30)
+knitr::kable(head(first_stage_keywords, 30),
+             digits = 2)
+knitr::kable(head(second_stage_keywords, 30),
+             digits=2)
 become_predictive<- p_change %>%
-  filter(stage2_1>.8) %>%
-  arrange(-logit_change)
+  filter(stg2_1>.8) %>%
+  arrange(-lgt_chng)
 
 become_less_predictive<- p_change %>%
-  filter(stage1_1>.8) %>%
-  arrange(logit_change)
-head(become_predictive, 30)
-head(become_less_predictive, 30)
+  filter(stg1_1>.8) %>%
+  arrange(lgt_chng)
+knitr::kable(head(become_predictive, 30),
+             digits = 2)
+
+knitr::kable(head(become_less_predictive, 30),
+             digits=2)
 
 
-#
+##----first.stage.classification.ocr----
+all_corpus_o<-corpus(all_docs[c("fakeid", "ocr", "EV_article", "classified")], text_field = "ocr")
+all_dfm_o <- preprocess_corpus(all_corpus_o, stem=TRUE, min_termfreq=20, min_docfreq = 20)
+class_dfm_o<-quanteda::dfm_subset(all_dfm_o, quanteda::docvars(all_dfm_d, "classified")==1)
+class_nb_ocr <- quanteda::textmodel_nb(class_dfm_o, y=quanteda::docvars(class_dfm_o, "EV_article"), prior="uniform")
+first_stage_keywords_ocr<-nb_keywords(class_dfm_o, "EV_article")
 
-
-##----First.stage.classifier----
-
-
-##----Second.stage.classifier----
+knitr::kable(head(first_stage_keywords_ocr, 30), digits = 2)
