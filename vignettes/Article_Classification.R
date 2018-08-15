5a1527d3960791b7d7b7bc3a3e150df1998f8243
Gidon Cohen <gidon.cohen@durham.ac.uk>
2018-08-15 14:19:30 +0100

    don't eval vignette

--- vignettes/Article_Classification.R
+++ vignettes/Article_Classification.R
@@ -1,6 +1,7 @@
 ## ----setup, include = FALSE----------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
+  eval=FALSE,
   comment = "#>"
 )
 
@@ -51,72 +52,72 @@ documents_to_latex<-function(out_docs, include_description=TRUE, include_ocr=TRU
 
 
 ## ----message=FALSE, warning=FALSE, echo=FALSE----------------------------
-library(durhamevp)
+#  library(durhamevp)
+#  
+#  ## also using tidyverse functions
+#  library(tidyverse)
 
-## also using tidyverse functions
-library(tidyverse)
+## ---- eval=FALSE---------------------------------------------------------
+#  candocs<-get_candidate_documents(status =c("0","1", "2", "4", "5", "6", "7", "8"), include_ocr=FALSE)
+#  
+#  candocs$EV_article<-ifelse(candocs$status %in% c("1", "3"), 1, 0)
+#  
 
-## ------------------------------------------------------------------------
-candocs<-get_candidate_documents(status =c("0","1", "2", "3", "4", "5", "6", "7", "8"), include_ocr=FALSE)
+## ---- eval=FALSE---------------------------------------------------------
+#  all_searches<-get_archivesearches()
+#  
 
-candocs$EV_article<-ifelse(candocs$status %in% c("1", "3"), 1, 0)
+## ---- eval=FALSE---------------------------------------------------------
+#  initial_1841_searches<-all_searches%>%
+#    dplyr::select(id, search_text, archive_date_start, archive_date_end) %>%
+#    filter(archive_date_start>lubridate::ymd("1841-01-01"), archive_date_start<lubridate::ymd("1841-12-31")) %>%
+#    filter(search_text %in% c("election", "candidate", "party", "husting", "magistrate",
+#                              "riot", "disturbance", "incident", "mob", "rough", "adjourn",
+#                              "prison", "police"))
+#  
+#  
 
+## ---- eval=FALSE---------------------------------------------------------
+#  res_i_1841<-get_archivesearchresults(archive_search_id = initial_1841_searches$id)
+#  dim(res_i_1841)
 
-## ------------------------------------------------------------------------
-all_searches<-get_archivesearches()
+## ---- eval=FALSE---------------------------------------------------------
+#  res_oneday_1832<-get_archivesearchresults(archive_search_id = c(228:255))
+#  
 
-
-## ------------------------------------------------------------------------
-initial_1841_searches<-all_searches%>%
-  dplyr::select(id, search_text, archive_date_start, archive_date_end) %>%
-  filter(archive_date_start>lubridate::ymd("1841-01-01"), archive_date_start<lubridate::ymd("1841-12-31")) %>%
-  filter(search_text %in% c("election", "candidate", "party", "husting", "magistrate",
-                            "riot", "disturbance", "incident", "mob", "rough", "adjourn",
-                            "prison", "police"))
-
-
-
-## ------------------------------------------------------------------------
-res_i_1841<-get_archivesearchresults(archive_search_id = initial_1841_searches$id) 
-dim(res_i_1841)
-
-## ------------------------------------------------------------------------
-res_oneday_1832<-get_archivesearchresults(archive_search_id = c(228:255))
-dim(res_oneday_1832)
-
-## ------------------------------------------------------------------------
-
-res_i_1841 <- res_i_1841 %>%
-  left_join(all_searches, by=c("archive_search_id"="id")) %>%
-  mutate(std_url = sub("download/", "", url))
-
-res_oneday_1832 <- res_oneday_1832 %>%
-  left_join(all_searches, by=c("archive_search_id"="id")) %>%
-  mutate(std_url = sub("download/", "", url))
-
-
-## ------------------------------------------------------------------------
-
-select_descript_xgb<-classifier_selection_description(candocs, res_i_1841, classifier_type = "xgboost")
-
-dim(select_descript_xgb)
-
+## ---- eval=FALSE---------------------------------------------------------
+#  
+#  res_i_1841 <- res_i_1841 %>%
+#    left_join(all_searches, by=c("archive_search_id"="id")) %>%
+#    mutate(std_url = sub("download/", "", url))
+#  
+#  res_oneday_1832 <- res_oneday_1832 %>%
+#    left_join(all_searches, by=c("archive_search_id"="id")) %>%
+#    mutate(std_url = sub("download/", "", url))
+#  
 
 ## ------------------------------------------------------------------------
-select_descript_nb<-classifier_selection_description(candocs, res_i_1841, classifier_type = "nb")
-dim(select_descript_nb)
-
+#  
+#  select_descript_xgb<-classifier_selection_description(candocs, res_i_1841, classifier_type = "xgboost")
+#  
+#  dim(select_descript_xgb)
+#  
 
 ## ------------------------------------------------------------------------
-xgb_cand_docs<-get_candidates_fromarchivesearchresults(select_descript_xgb)
-knitr::kable(head(xgb_cand_docs[xgb_cand_docs$status!="1", c("status", "description")]))
+#  select_descript_nb<-classifier_selection_description(candocs, res_i_1841, classifier_type = "nb")
+#  dim(select_descript_nb)
+#  
 
-xgb_cand_docs$status<-1
-xgb_cand_docs$g_status<-""
-xgb_cand_docs$status_writer<-"xgboost_description"
-
-
-
+## ------------------------------------------------------------------------
+#  xgb_cand_docs<-get_candidates_fromarchivesearchresults(select_descript_xgb)
+#  knitr::kable(head(xgb_cand_docs[xgb_cand_docs$status!="1"|is.na(xgb_cand_docs$status), c("status", "description")]))
+#  
+#  xgb_cand_docs$status<-1
+#  xgb_cand_docs$g_status<-""
+#  xgb_cand_docs$status_writer<-"xgboost_description"
+#  
+#  
+#  
 
 ## ---- eval=FALSE---------------------------------------------------------
 #  to_csv<-xgb_cand_docs[,c("id", "url", "publication_title", "description", "status", "g_status", "title", "status_writer")]
@@ -125,6 +126,6 @@ xgb_cand_docs$status_writer<-"xgboost_description"
 #  write.csv(to_csv, file=csv_filename)
 
 ## ---- results='asis'-----------------------------------------------------
-documents_to_latex(sample_n(select_descript_xgb, 30), include_ocr=FALSE, pandoc_output=TRUE)
-
+#  documents_to_latex(sample_n(select_descript_xgb, 30), include_ocr=FALSE, pandoc_output=TRUE)
+#  
 
