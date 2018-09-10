c7f8f74a445fdd709a957bfbafe7b5ee5f4cb70a
GaryHutchison1611 <gary.d.hutchison@durham.ac.uk>
2018-09-10 09:42:02 +0100

    Initial commit

---
+++ other_R_code/database_general_gdh.R
@@ -0,0 +1,167 @@
+# Initial Setup (DEFUNCT)
+install.packages('data.table')
+install.packages('knitr')
+install.packages('miniUI')
+install.packages('shiny')
+install.packages("pool")
+install.packages("magrittr")
+install.packages("dplyr")
+devtools::install_github("rstudio/pool")
+
+# Redownload durhamevp
+devtools::install_github("gidonc/durhamevp")
+
+# Initial Setup (ACTIVE)
+#keyring::key_set("evp", username="data_writer")
+library(magrittr)
+library(shiny)
+library(pool)
+library(tidyverse)
+library(dplyr)
+library(durhamevp)
+con1 <- evdb_connect(password_method = "keyring") # connect to database and save connection as 'con'
+con1
+.evp_db_pool <- NULL
+
+
+# Get user details for user X
+get_user(10)
+userinfo<-get_user(10)
+View(userinfo)
+
+
+# To assign article id X to user Y
+assign_article_to_user(688, 7, allocation_type = "training", allocated_by="testing assignment")
+
+
+# View individual user allocations (doc numbers only)
+user_allocated_fromset(9, set = define_testset(), not_allocated = FALSE)
+user7allocations <- user_allocated_fromset(9, set = define_testset(), not_allocated = FALSE)
+View(user7allocations)
+
+
+# reassign userdoc allocations
+reallocate_randomly(c(9, 16, 21), 4553:4860,
+                    allocated_by = "random_reassignment", restrict_to_actual = TRUE,
+                    make_assignments = TRUE, force = FALSE)
+
+
+usergroupresults2 <- get_allocation(8:47, allocation_type = "coding", document_id = 2113:2368)
+View(usergroupresults2)
+
+
+# View individual user allocations (with scores)
+usergroupresults <- get_allocation(33, allocation_type = "coding", document_id = "all")
+View(usergroupresults %>% filter(status == "NEW"))
+View(usergroupresults %>% filter(status == "COMPLETED"))
+View(usergroupresults)
+
+# View xgboost results
+usergroupresults <- get_allocation(8, allocation_type = "coding", document_id = 3170:3608)
+View(usergroupresults %>% filter(status == "COMPLETED"))
+
+View(usergroupresults %>% filter(status == "NEW"))
+View(usergroupresults)
+
+## - unused, for later
+View(usergroupresults %>%
+       group_by(user_id, allocation_type, status)%>%
+       summarize(mean(score)))
+
+
+# View Individual User Metadata
+get_user(7)
+user7 <- get_user(7)
+View(user7)
+
+
+# Switch coders from training-testing, testing-coding, coding-checking
+set_user_mode (10,  new_mode = "training")
+
+
+#Assign test set to user(s)
+assign_testset_to_user(48, allocation_type = "testing",
+                       allocated_by = "assign_testset_to_user")
+
+#Assign training set to user(s)
+assign_trainingset_to_user(48, allocation_type = "training",
+                           allocated_by = "assign_testset_to_user")
+
+# Give users individual articles
+assign_article_to_user(19:47, allocation_type = "training", allocated_by = "GaryH", allocation_date = as.character(Sys.Date()), status = "NEW", coding_complete = 0)
+
+
+# To see Gary's codings of all X's codings
+jb_coding<-get_allocation(8, allocation_type="testing")
+jb_doc_ids<-jb_coding$document_id
+GaryVsX<-get_allocation(6, document_id = jb_doc_ids)
+View(GaryVsX)
+
+
+## To see comparison coding ids for test set results
+View(GaryVsX[,c("id", "document_id")])
+
+
+# See general times spent by coders
+activity<-DBI::dbGetQuery(con, 'select *
+                          from portal_useractivitylogentry')
+activity %>%
+  #group_by(user_id) %>%
+  filter(user_id>7) %>%
+  group_by(user_id, allocation_id) %>%
+  summarize(start=min(date), end=max(date), time_take=difftime(end, start)) %>%
+  ggplot(aes(time_take))+
+  facet_wrap(~user_id)+
+  geom_histogram()
+
+
+# View Individual Documents
+get_document(819)
+indiv_doc <- get_document(819)
+View(indiv_doc)
+
+
+#View Individual Candidate Documents
+candidate_docs<-get_candidate_documents(cand_document_id = "8135")
+View(candidate_docs)
+
+
+#View Individual Candidate Documents WITH STATUS
+candidate_docs<-get_candidate_documents(cand_document_id = 1000:15000)
+filter(candidate_docs, status == 10)
+filter(id)
+View(candidate_docs)
+
+
+# How to see archive searches, various ways of pruning/combining results
+get_archivesearches(703)
+get_archivesearches("all")
+aa<-get_archivesearches(675:687)
+View(aa)
+bb<-get_archivesearchresults(704)
+View(bb)
+View(head(bb))
+cc<-left_join(bb, aa, by=c("archive_search_id"="id"))
+View(cc)
+View(cc[cc$archive_search_id==71,])
+cc[5, "url"]
+duplicated(cc$url)
+duplicated(cc$url)
+
+
+# Sandbox
+
+
+allocations <- get_allocation
+is_complete <- allocations %>%
+  filter(status=="COMPLETED", user_id==7)
+View(allocations)
+with_score <- allocations %>%
+  filter(score!=-1)
+docs<-get_document(document_id="all")
+View(docs)
+
+
+eventreport <- get_event_report("ALL")
+View(eventreport)
+
