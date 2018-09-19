64cd28090539e47a8dedcafc05d4477d87b5327f
GaryHutchison1611 <gary.d.hutchison@durham.ac.uk>
2018-09-19 11:17:45 +0100

    Xgboost 1847 tranche generation.

--- other_R_code/xgboost_process_gdh.R
+++ other_R_code/xgboost_process_gdh.R
@@ -201,3 +201,56 @@ getwd()
 
 # Write different sort of csv - see the descriptions the classifier is basing its judgement on
 write.csv(select_descript_xgb4[sample(nrow(select_descript_xgb4)),c("url", "description")], "checkit4.csv", row.names=FALSE)
+
+
+
+
+
+
+# Machine Learning 1847
+candocs<-get_candidate_documents(g_status =c("0","1", "2", "4", "5", "6", "7", "8"), include_ocr=FALSE)
+
+candocs$EV_article<-ifelse(candocs$g_status %in% c("1", "3"), 1, 0)
+
+all_searches<-get_archivesearches()
+
+initial_1847_searches<-all_searches%>%
+  dplyr::select(id, search_text, archive_date_start, archive_date_end) %>%
+  filter(archive_date_start>lubridate::ymd("1847-07-27"), archive_date_start<lubridate::ymd("1847-08-29")) %>%
+  filter(search_text %in% c("election", "candidate", "party", "husting", "magistrate",
+                            "riot", "disturbance", "incident", "mob", "rough", "adjourn",
+                            "prison", "police"))
+
+res_i_1847<-get_archivesearchresults(archive_search_id = initial_1847_searches$id)
+dim(res_i_1847)
+
+res_i_1847 <- res_i_1847 %>%
+  left_join(all_searches, by=c("archive_search_id"="id")) %>%
+  mutate(std_url = sub("download/", "", url))
+
+select_descript_xgb5<-classifier_selection_description(candocs, res_i_1847, classifier_type = "xgboost")
+
+dim(select_descript_xgb5)
+
+xgb_cand_docs<-get_candidates_fromarchivesearchresults(select_descript_xgb5)
+
+
+knitr::kable(head(xgb_cand_docs[xgb_cand_docs$g_status!="1"|is.na(xgb_cand_docs$status), c("status", "g_status", "description")]))
+
+xgb_cand_docs$status<-1
+
+xgb_cand_docs$status_writer<-"xgboost_description"
+
+
+
+to_csv<-xgb_cand_docs[,c("id", "url", "publication_title", "description", "status", "g_status", "title", "status_writer")]
+csv_filename<-gsub(" ", "_", paste0("britishnewspaperarchive_", Sys.time(), ".csv"))
+csv_filename<-gsub(":", "_", csv_filename)
+write.csv(to_csv, file=csv_filename, row.names=FALSE)
+
+getwd()
+
+
+
+# Write different sort of csv - see the descriptions the classifier is basing its judgement on
+write.csv(select_descript_xgb5[sample(nrow(select_descript_xgb5)),c("url", "description")], "checkit5.csv", row.names=FALSE)
