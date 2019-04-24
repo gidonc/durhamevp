ba573a7786f08de9c1cf1e33ce45044769ba6261
Gidon Cohen <gidon.cohen@durham.ac.uk>
2019-04-24 23:00:48 +0100

    add function to find if a month contains an election

--- R/define_sets.R
+++ R/define_sets.R
@@ -39,4 +39,33 @@ date_to_election<-function(the_date, election_start_col="monthsearch_start", ele
 
 }
 
+month_contains_election<-function(the_date, election_start_col="monthsearch_start", election_end_col="monthsearch_end", election_data=durhamevp::election_dates, return_col="election_name"){
+  #' Finds the election relating to a month (where an election took place on any day that month).
+  #'
+  #'
+  #'@param the_date the month to classifiy as yyyy-mm-dd where dd must be 01.
+  #'@param election_start_col the column in the election data frame which identifies when the election starts.
+  #'@param election_end_col the column in the election data frame which identifies when the election ends.
+  #'@param election_data the data frame containing the election data.
+  #'@param return_col the column in the data frame containing the name of the election (to be returned by the function).
+  #'@export
+
+  the_date<-lubridate::ymd(the_date)
+  if(lubridate::day(the_date)!=1){
+    stop("Day not first of the month")
+  }
+  n_days<-lubridate::days_in_month(the_date)
+  dates_in_month <- 1:n_days
+  dates_in_month <- rep(the_date, n_days)
+  lubridate::day(dates_in_month)<-1:n_days
+  res<-date_to_election(dates_in_month)
+  if(sum(is.na(res)==length(res))){
+    res<-NA
+  } else {
+    res<-res[!is.na(res)][1]
+  }
+
+  res
+}
+
 
