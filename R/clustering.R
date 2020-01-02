25424108c1dd049dd5650715c19a5de7376759d8
Gidon Cohen <gidon.cohen@durham.ac.uk>
2020-01-02 22:25:05 +0000

    Update autoclustering function to reflect new database structure

--- R/clustering.R
+++ R/clustering.R
@@ -37,7 +37,7 @@ assign_eventreport_to_autocluster <- function(autodetected_cluster_id, event_rep
 }
 
 
-assign_autocluster_to_user <- function(autodetected_cluster_id, user_id, allocation_date=as.character(Sys.Date()), last_updated=as.character(Sys.time()), completed=0){
+assign_autocluster_to_user <- function(autodetected_cluster_id, user_id, allocation_date=as.character(Sys.Date()), last_updated=as.character(Sys.time()), completed=0, needs_additional_checks=0){
   #' Assign autodetected cluster to a user.
   #'
   #' \code{assign_autocluster_to_user} assigns an autodetected cluster to a user for verification.
@@ -46,19 +46,21 @@ assign_autocluster_to_user <- function(autodetected_cluster_id, user_id, allocat
   #' @param allocation_date Date allocation made (usually today).
   #' @param last_updated Date record last updated (usually today).
   #' @param completed Is the cluster verification complete (usually 0 [i.e. not complete])
+  #' @para needs_additional_checks Does the cluster need additional checks (for a new cluster allocation should be zero - may be changed later on the platform as part of the process)
 
   #' @export
 
   con <- manage_dbcons()
 
-  this_sql <- "INSERT INTO  portal_userautodetectedclusterallocation (autodetected_cluster_id, user_id, allocation_date, last_updated, completed) VALUES (?autodetected_cluster_id, ?user_id, ?allocation_date, ?last_updated, ?completed) ;"
+  this_sql <- "INSERT INTO  portal_userautodetectedclusterallocation (autodetected_cluster_id, user_id, allocation_date, last_updated, completed, needs_additional_checks) VALUES (?autodetected_cluster_id, ?user_id, ?allocation_date, ?last_updated, ?completed, ?needs_additional_checks) ;"
   this_safe_sql <- DBI::sqlInterpolate(DBI::ANSI(),
                                        this_sql,
                                        autodetected_cluster_id = autodetected_cluster_id,
                                        user_id = user_id,
                                        allocation_date = allocation_date,
                                        last_updated=last_updated,
-                                       completed = completed)
+                                       completed = completed,
+                                       needs_additional_checks= needs_additional_checks)
 
   DBI::dbGetQuery(con, this_safe_sql)
 }
