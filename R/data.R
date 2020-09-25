bfa69ab88586eba0d01dd0a8947b3c9cebf21c64
Gidon Cohen <gidon.cohen@durham.ac.uk>
2020-09-25 09:58:29 +0100

    Just showing Nick something

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' A dataset containing the dates and other basic information about British General Elections between 1832 and 1910.
 #'
-#' @format A data frame with 20 rows and 11 variables:
+#' @format A data frame with 20 rows and 11 variables, which are:
 #' \describe{
 #'   \item{election_name}{The standardized name of the election,used to link to other data sets in the election violence project.}
 #'   \item{keywords_13_complete}{Indicates whether the projects the BNA search using the standard 13 keywords to detect election violnece has been completed.}
